d4401436488fce628dcefa90feb16bee07ca6442
Coline Peetermans <colinepeetermans@MacBook-Pro-de-Coline.local>
2022-11-16 16:33:07 +0100

    setup project

---
+++ src/components/molecules/PageHeader.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="header">
+        <img src="@/assets/img/logo.png" alt="" width="100">
+    </div>
+</template>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,50 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    quote: null
   },
   getters: {
   },
-  mutations: {
-  },
   actions: {
+    async fetchQuote({commit}) {
+      try {
+        const config = {
+          method: 'POST',
+          url: 'https://staging-gtw.seraphin.be/quotes/professional-liability',
+          headers: {
+            'X-Api-Key': 'fABF1NGkfn5fpHuJHrbvG3niQX6A1CO53ftF9ASD',
+            'Content-Type': 'application/json'
+          },
+          data: {
+            "annualRevenue": 80000,
+            "enterpriseNumber": "0649885171",
+            "legalName": "example SA",
+            "naturalPerson": true,
+            "nacebelCodes": ["62010", "62020", "62030", "62090", "63110"]
+          }
+        }
+
+        const response = await axios(config)
+        if (response.data) {
+          console.log(response.data)
+          commit('SET_QUOTE', response.data.data)
+        } 
+        // console.log(response)
+       
+
+      } catch (error) {
+        console.log(error)
+      }
+
+    },
   },
-  modules: {
-  }
-})
+  mutations: {
+    SET_QUOTE(state, data) {
+      state.quote = data
+      console.log(state.quote)
+    }
+  },
+},
+)
